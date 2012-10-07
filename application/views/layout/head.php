0bb26f82d0e61873eaff5cfd3787b2b3790c25d3
Paul-Willy Jean <paul1993willy@gmail.com>
2012-10-06 21:13:39 -0400

    Conception de base du head de la page.

---
+++ application/views/layout/head.php
@@ -0,0 +1,7 @@
+<?php echo Kohana::FILE_SECURITY ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Association Générale Étudiante de Bois-de-Boulogne</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="" />
+<?php echo HTML::style("asset/css/global.css") ?>
