b0a39f91312adab76478cec24a6b863d07aac420
Paul-Willy Jean <paul1993willy@gmail.com>
2012-10-22 14:51:02 -0400

    Ajout d'informations par defaut au template de l'age

--- application/classes/controller/template/age.php
+++ application/classes/controller/template/age.php
@@ -7,6 +7,7 @@ class Controller_Template_Age extends Controller_Template {
 	protected $description;
 	protected $keywords;
 
+	public $header;
 	protected $body;
 
 	public function before ()
@@ -18,25 +19,26 @@ class Controller_Template_Age extends Controller_Template {
 		View::bind_global('keywords', $this->keywords);
 
 		$this->title = isset($this->title) ?
-			'AGEBdeB - ' . $this->title :
-			'AGEBdeB';
+			'AGEBdeB - ' . $this->title : 'AGEBdeB';
 
 		$this->description = isset($this->description) ?
-			$this->description : '';
+			$this->description : 'Le site de l\'association générale étudiante
+				de bois de boulogne';
 
 		$this->keywords = isset($this->keywords) ?
-		$this->keywords : '';
+			$this->keywords : '';
+		$this->head = View::factory('layout/head');
+		$this->header = View::factory('layout/header');
 	}
 
 	public function action_index()
 	{
-		$accueil = View::factory('accueil', NULL);
-		$this->response->body($accueil);
 	}
 	
 	public function after ()
 	{
-
+		$this->body = $this->head . $this->header . $this->body;
+		$this->response->body($this->body);
 	}
 } // End Welcome
 ?>
--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -1,7 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Association Générale Étudiante de Bois-de-Boulogne</title>
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="author" content="" />
-<?php echo HTML::style("asset/css/global.css") ?>
+<!Doctype html>
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php echo $title ?></title>
+	<meta name="description" content="<?php echo $description ?>" />
+	<meta name="keywords" content="<?php echo $keywords ?>" />
+	<?php echo HTML::style('asset/css/reset.css') ?>
+	<?php echo HTML::style("asset/css/global.css") ?>
