8b305bbdd0c794bc12799eea5e073bdba8d929c5
Paul-Willy Jean <paul1993willy@gmail.com>
2012-10-22 14:53:39 -0400

    Debut du squelette du menu de navigation

---
+++ application/views/layout/header.php
@@ -0,0 +1,33 @@
+<header>
+	<div id="logo">
+		
+	</div>
+	<nav>
+		<ul>
+			<li>Lien 1</li>
+			<li>
+				<ul>
+					<li>Sous-lien 1</li>
+					<li>Sous-lien 2</li>
+					<li>Sous-lien 3</li>
+				</ul>
+			</li>
+			<li>Lien 2</li>
+			<li>
+				<ul>
+					<li>Sous-lien 1</li>
+					<li>Sous-lien 2</li>
+					<li>Sous-lien 3</li>
+				</ul>
+			</li>
+			<li>Lien 3</li>
+			<li>
+				<ul>
+					<li>Sous-lien 1</li>
+					<li>Sous-lien 2</li>
+					<li>Sous-lien 3</li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</header>
