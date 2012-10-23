3a243977c76cb0cab95c6356e7f4e5f1ccde73d5
Paul-Willy Jean <paul-willy@gmail.com>
2012-10-23 00:14:48 -0400

    Meilleure interpretation des sous-menus

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,27 +1,27 @@
 <header>
 	<div id="logo">
-		
+		<?php echo HTML::image('asset/image/agebdeb_logo.jpg', array('alt' => 'logo de l\'AGEBdeB')); ?>	
 	</div>
 	<nav>
 		<ul>
-			<li>Lien 1</li>
 			<li>
+				Lien 1
 				<ul>
 					<li>Sous-lien 1</li>
 					<li>Sous-lien 2</li>
 					<li>Sous-lien 3</li>
 				</ul>
 			</li>
-			<li>Lien 2</li>
 			<li>
+				Lien 2
 				<ul>
 					<li>Sous-lien 1</li>
 					<li>Sous-lien 2</li>
 					<li>Sous-lien 3</li>
 				</ul>
 			</li>
-			<li>Lien 3</li>
 			<li>
+				Lien 3
 				<ul>
 					<li>Sous-lien 1</li>
 					<li>Sous-lien 2</li>
