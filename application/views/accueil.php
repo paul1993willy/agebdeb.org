869fe169a9d64fc8be14481b099a5f2d87e50cab
Paul-Willy Jean <paul1993willy@gmail.com>
2012-11-12 10:08:42 -0500

    Arrangement visuel, ajout de contenu et debut de validation
    - Arrange le visuel pour conserver une certaine coherence
    - Ajoute du contenu pour voir ce a quoi ca ressemblera
    - Ajoute les tags d'ouverture des balises html et body

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -5,6 +5,7 @@
 			<p>
 				Du contenu que je devrai remplir avec du lorem ipsum
 			</p>
+			<time pubdate="pubdate"><?php echo Date::formatted_time('now', 'H:i:s') ?></time>
 		</article>
 	</section>
 </div>
--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -1,10 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 <!Doctype html>
 <html>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title><?php echo $title ?></title>
-	<meta name="description" content="<?php echo $description ?>" />
-	<meta name="keywords" content="<?php echo $keywords ?>" />
-	<?php echo HTML::style('asset/css/reset.css') ?>
-	<?php echo HTML::style("asset/css/global.css") ?>
-	<?php echo HTML::style("asset/css/theme.css") ?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title><?php echo $title ?></title>
+		<meta name="description" content="<?php echo $description ?>" />
+		<meta name="keywords" content="<?php echo $keywords ?>" />
+		<?php echo HTML::style('asset/css/reset.css') ?>
+		<?php echo HTML::style("asset/css/global.css") ?>
+		<?php echo HTML::style("asset/css/theme.css") ?>
+	</head>
