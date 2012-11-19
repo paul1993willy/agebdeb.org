613a84564ee7fdbb2cd3c8105fee16acc34eba8d
Paul-Willy Jean <paul1993willy@gmail.com>
2012-11-19 00:26:36 -0500

    Ajout du calendrier

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -14,14 +14,36 @@
 			Veuillez faire un tour au local de votre association ! S-159
 		</p>
 	</section>
-	<section class="span4">
+	<section class="span4 minimized pull-left">
 		<h1>Actualité</h1>
 		<?php for($i = 0; $i < 3; $i++) : ?>
-		<article class="minimized">
+		<article>
 			<p>
 				<time datetime="<?php echo Date::formatted_time('now', 'Y-m-d') ?>" pubdate="pubdate"><?php echo Date::formatted_time('now', 'd.m.Y') ?></time> Du contenu que je devrai remplir avec du lorem ipsum asdfadf asdf asdf sadf asdf asdf asdf asdf sad fasd fas dffas df
 			</p>
 		</article>
 		<?php endfor; ?>
+	</section>
+	<section class="minimized span2 pull-left">
+		<h1>Calendrier</h1>
+		<table>
+			<thead>
+				<tr>
+					<th colspan="7"><?php echo Date::formatted_time('now', 'F Y') ?></th>
+				</tr>
+				<tr>
+					<th>Dim</th>
+					<th>Lun</th>
+					<th>Mar</th>
+					<th>Mer</th>
+					<th>Jeu</th>
+					<th>Ven</th>
+					<th>Sam</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr></tr>
+			</tbody>
+		</table>
 	</section>
 </div>
