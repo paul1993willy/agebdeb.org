d227706d39b6efcbb22ede0449ce5160b6707dac
Paul-Willy Jean <paul1993willy@gmail.com>
2012-12-27 03:29:26 -0500

    Modifie l'en-tete et ajoute une nouvelle police de caractere

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,5 +1,12 @@
 <header>
-	<div id="logo">
-		<?php echo HTML::image('asset/image/agebdeb_logo.jpg', array('alt' => 'logo de l\'AGEBdeB')); ?>	
+	<div id="logo" class="pull-left">
+		<?php echo HTML::image('asset/image/agebdeb.org/logo-age.svg', array('alt' => 'logo de l\'AGEBdeB')); ?>	
 	</div>
+	<div id="sigle">
+		AGEBdeB
+	</div>
+	<div id="nom_asso">
+		Association Générale<br> Étudiante de<br> Bois-de-Boulogne
+	</div>
+	<div class="clear"></div>
 </header>
