87eec1177e885fff0ecbe3c363118c139ce2a7d3
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-04-16 22:29:22 -0400

    Ajouté le logo de l'age dans l'en-tête. Ajouté un module de cms

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,12 +1,19 @@
 <?php echo View::factory("menu/principal") ?>
 
-<header class="header hero-unit" style="background: url('<?php echo URL::site("asset/image/agebdeb.org/background.gif") ?>');">
+<header class="header hero-unit">
 
     <div class="container">
-
-        <h1>AGEBdeB</h1>
-
-        <p>Association Générale Étudiante du collège de Bois-de-Boulogne</p>
+        <div class="row">
+            <div class="span10">
+                <h1>AGEBdeB</h1>
+        
+                <p>Association Générale Étudiante du collège de Bois-de-Boulogne</p>
+                
+            </div>
+            <div class="span2">
+                <?php echo HTML::image("asset/img/logo-age.svg", NULL, array("class" => "span3")) ?>
+            </div>
+        </div>
 
     </div>
 
